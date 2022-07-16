682cc8041c71b5e18d0d449b8dd6b7e400e4566c
loibq <buiquangloi1993@gmail.com>
2022-07-17 00:32:12 +0700

    update command permission

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@
 cd /var/www/html/ && \
 composer install && \
 composer dumpautoload && \
+sudo chown -R nginx:ec2-user /var/www/html/* && \
+sudo find /var/www/html/ -type f -exec chmod 664 {} \; && \
+sudo find /var/www/html/ -type d -exec chmod 775 {} \; && \
 php artisan migrate --force && \
 php artisan cache:clear && \
 php artisan config:clear && \
